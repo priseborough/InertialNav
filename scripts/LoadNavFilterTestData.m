1dfcc2631f4ae0a04496332792e3d91fb712202f
priseborough <p_riseborough@live.com.au>
2013-12-09 20:52:36 +1100

    Data Loading : Modified to provide angular rates and accelerations and better time data

--- scripts/LoadNavFilterTestData.m
+++ scripts/LoadNavFilterTestData.m
@@ -4,22 +4,17 @@ clear all;
 load('TestLog3.mat')
 %% IMU Data
 
-for i = 1:length(IMU)-1
-    dAngX(i) = 0.5*(IMU(i+1,3)+IMU(i,3))*(IMU(i+1,2)-IMU(i,2))*1e-3;
-    dAngY(i) = 0.5*(IMU(i+1,4)+IMU(i,4))*(IMU(i+1,2)-IMU(i,2))*1e-3;
-    dAngZ(i) = 0.5*(IMU(i+1,5)+IMU(i,5))*(IMU(i+1,2)-IMU(i,2))*1e-3;
-    dVelX(i) = 0.5*(IMU(i+1,6)+IMU(i,6))*(IMU(i+1,2)-IMU(i,2))*1e-3;
-    dVelY(i) = 0.5*(IMU(i+1,7)+IMU(i,7))*(IMU(i+1,2)-IMU(i,2))*1e-3;
-    dVelZ(i) = 0.5*(IMU(i+1,8)+IMU(i,8))*(IMU(i+1,2)-IMU(i,2))*1e-3;
-    IMUframe(i) = IMU(i+1,1);
-    IMUtime(i) = (IMU(i+1,2)-IMU(2,2))*1e-3;
-end
+IMUframe = IMU(:,1);
+IMUtime  = IMU(:,2)*1e-3;
+angRate  = IMU(:,3:5);
+accel    = IMU(:,6:8);
 
 %% GPS Data
 
 if ~isempty(GPS)
     goodDataIndices=find(GPS(:,2) == 3);
     GPSframe = GPS(goodDataIndices,1);
+    GPStime = GPS(goodDataIndices,3);
     LatDeg = GPS(goodDataIndices,7);
     LngDeg = GPS(goodDataIndices,8);
     Hgt = GPS(goodDataIndices,9);
@@ -33,6 +28,7 @@ end
 
 if ~isempty(MAG)
     MAGframe = MAG(:,1);
+    MAGtime  = MAG(:,2)*1e-3;
     MagX     = MAG(:,3) - MAG(:,6);
     MagY     = MAG(:,4) - MAG(:,7);
     MagZ     = MAG(:,5) - MAG(:,8);
@@ -45,6 +41,7 @@ end
 
 if ~isempty(NTUN)
     ADSframe = NTUN(:,1);
+    ADStime  = NTUN(:,2)*1e-3;
     Veas     = NTUN(:,8);
     HgtBaro  = NTUN(:,9);
 end
@@ -53,17 +50,19 @@ end
 
 if ~isempty(ATT)
     ATTframe = ATT(:,1);
+    ATTtime  = ATT(:,2)*1e-3;
     Roll     = ATT(:,3)*deg2rad;
     Pitch    = ATT(:,4)*deg2rad;
     Yaw      = ATT(:,5)*deg2rad;
 end
 
 %% Save to file
-save('NavFilterTestData.mat','dAngX','dAngY','dAngZ','dVelX','dVelY','dVelZ','IMUframe','IMUtime','GPSframe',...
-    'LatDeg','LngDeg','Hgt','CourseDeg','GndSpd','VelD',...
-    'MAGframe','MagX','MagY','MagZ','MagBiasX','MagBiasY','MagBiasZ',...
-    'ATTframe','Roll','Pitch','Yaw', ...
-    'ADSframe','Veas','HgtBaro');
+save('NavFilterTestData.mat', ...
+    'IMUframe','IMUtime','angRate','accel', ...
+    'GPSframe','GPStime','LatDeg','LngDeg','Hgt','CourseDeg','GndSpd','VelD', ...
+    'MAGframe','MAGtime','MagX','MagY','MagZ','MagBiasX','MagBiasY','MagBiasZ', ...
+    'ATTframe','ATTtime','Roll','Pitch','Yaw', ...
+    'ADSframe','ADStime','Veas','HgtBaro');
 
 clear all;
 load('NavFilterTestData.mat');
