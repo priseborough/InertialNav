e021b0a9fe7514eb318eef57120e119074d3caf7
Lorenz Meier <lm@inf.ethz.ch>
2014-09-02 00:59:38 +0200

    More testing and tools

--- code/plot_rng.py
+++ code/plot_rng.py
@@ -7,7 +7,7 @@ import numpy as np
 import math
 
 data = np.genfromtxt('RngFuse.txt', delimiter=' ', skip_header=1,
-        skip_footer=1, names=['time', 'IRNG', 'VRNG'])
+        skip_footer=1, names=['time', 'IRNG', 'VRNG', 'EST', 'DIST', 'BARO'])
 
 figTas = plt.figure()
 
@@ -17,8 +17,12 @@ ax1.set_title("Range Finder Innovations")
 ax1.set_xlabel('time (s)')
 ax1.set_ylabel('range (m)')
 #ax1.set_ylim([-0.0025,0.0025])
-ax1.plot(data['time'], data['IRNG'], color='b', label='Range')
+ax1.plot(data['time'], data['IRNG'], color='b', label='Range Innovations')
 ax1.plot(data['time'], SRNG, color='r')
 ax1.plot(data['time'], -SRNG, color='r')
+ax1.plot(data['time'], data['EST'], color='g', label='Ground altitude estimate')
+ax1.plot(data['time'], data['DIST'], color='m', label='Raw distance measurement')
+ax1.plot(data['time'], data['BARO'], color='black', label='Barometric altitude')
+ax1.legend()
 
 plt.show()
