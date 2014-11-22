dfa78e345be58ae447e93d4d7e94840070339fa4
priseborough <p_riseborough@live.com.au>
2014-11-23 06:42:52 +1100

    Increase optical flow measurement noise variance

--- code/estimator_23states.h
+++ code/estimator_23states.h
@@ -74,7 +74,7 @@ public:
         accelProcessNoise = 0.5f;
 
         gndHgtSigma  = 0.1f; // terrain gradient 1-sigma
-        R_LOS = 0.03f; // optical flow measurement noise variance (rad/sec)^2
+        R_LOS = 0.09f; // optical flow measurement noise variance (rad/sec)^2
         flowInnovGate = 3.0f; // number of standard deviations applied to the optical flow innovation consistency check
         auxFlowInnovGate = 10.0f; // number of standard deviations applied to the optical flow innovation consistency check used by the auxiliary filter
         rngInnovGate = 5.0f; // number of standard deviations applied to the rnage finder innovation consistency check
